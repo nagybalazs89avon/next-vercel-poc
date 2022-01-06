10bf3724d59d2d43074d8903c5be5c8921af92b7
nagybalazs89avon <balazs.nagy@avon.com>
2022-01-06 12:02:45 +0100

    Remove title

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to{" "}
-          <a href="https://nextjs.org">
-            Next.js! (Preview from an other feature branch)
-          </a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
