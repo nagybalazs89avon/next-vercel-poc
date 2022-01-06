68f3b04cb8912add7d826dd142a6e7250710b0de
nagybalazs89avon <balazs.nagy@avon.com>
2022-01-06 11:50:37 +0100

    Add some more details to title

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to{" "}
           <a href="https://nextjs.org">
-            Next.js! (Preview from an other branch)
+            Next.js! (Preview from feature branch)
           </a>
         </h1>
 
